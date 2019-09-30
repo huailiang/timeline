37f52f458025086f2a0fe68e9d8057616421434d
penghuailiang <penghuailiang@126.com>
2019-09-30 19:50:02 +0800

    timeline

--- Assets/com.unity.timeline/Runtime/External/AnchorBehaviour.cs
+++ Assets/com.unity.timeline/Runtime/External/AnchorBehaviour.cs
@@ -21,9 +21,10 @@ namespace UnityEngine.Timeline
             {
                 m_Transf = bindObj.transform;
             }
+            debugY = 0;
         }
 
-
+        float debugY = 0;
         public override void PrepareFrame(Playable playable, FrameData info)
         {
             base.PrepareFrame(playable, info);
@@ -31,12 +32,18 @@ namespace UnityEngine.Timeline
             if (clips_pos != null && direcor != null)
             {
                 float time = (float)(direcor.time);
+
                 if (m_Transf)
                 {
                     float x = clips_pos[0].Evaluate(time);
                     float y = clips_pos[1].Evaluate(time);
                     float z = clips_pos[2].Evaluate(time);
                     Vector3 pos = new Vector3(x, y, z);
+                    if (y > debugY)
+                    {
+                        debugY = y;
+                        Debug.Log(string.Format("debug {0:.00} time:{1:.00}", debugY, time));
+                    }
                     x = clips_rot[0].Evaluate(time);
                     y = clips_rot[1].Evaluate(time);
                     z = clips_rot[2].Evaluate(time);
